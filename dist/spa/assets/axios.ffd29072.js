c9f243d50048df30d5d88e861304375544cc2f53
Mark-Bazylev <138299775+Mark-Bazylev@users.noreply.github.com>
2024-04-06 21:22:25 +0300

    build: dist added

---
+++ dist/spa/assets/axios.ffd29072.js
@@ -0,0 +1,1 @@
+import{j as i}from"./index.737d3221.js";import{a}from"./axios.37d53be1.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
---
+++ dist/spa/assets/use-quasar.8362b04f.js
@@ -0,0 +1,1 @@
+import{i as a,ae as r}from"./index.737d3221.js";function s(){return a(r)}export{s as u};
